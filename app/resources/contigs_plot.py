261e2d79476ec519db8f09563787fa69bb364cdb
Mo Char <mocharrout@gmail.com>
2016-05-26 17:05:31 +0200

    load bin_set once in contigs_plot.py

--- app/resources/contigs_plot.py
+++ app/resources/contigs_plot.py
@@ -26,22 +26,16 @@ def find_gcs(contigs):
     return data
 
 
-def create_length_data(assembly, bin_set_id=None):
-    if bin_set_id is None:
-        return find_lengths(assembly.contigs)
-    bin_set = assembly.bin_sets.filter_by(id=bin_set_id).first()
+def create_length_data(assembly, bin_set=None):
     if bin_set is None:
-        abort(404)
+        return find_lengths(assembly.contigs)
     data = {bin.name: find_lengths(bin.contigs) for bin in bin_set.bins}
     return data
     
 
-def create_gc_data(assembly, bin_set_id=None):
-    if bin_set_id is None:
-        return find_gcs(assembly.contigs)
-    bin_set = assembly.bin_sets.filter_by(id=bin_set_id).first()
+def create_gc_data(assembly, bin_set=None):
     if bin_set is None:
-        abort(404)
+        return find_gcs(assembly.contigs)
     data = {bin.name: find_gcs(bin.contigs) for bin in bin_set.bins}
     return data
 
@@ -55,5 +49,10 @@ class ContigsPlotApi(Resource):
     def get(self, assembly_id):
         args = self.reqparse.parse_args()
         assembly = user_assembly_or_404(assembly_id)
-        return {'length': create_length_data(assembly, args.bs),
-                'gc': create_gc_data(assembly, args.bs)}
+        bin_set = None
+        if args.bs:
+            bin_set = assembly.bin_sets.filter_by(id=args.bs).first()
+            if bin_set is None:
+                abort(404)
+        return {'length': create_length_data(assembly, bin_set),
+                'gc': create_gc_data(assembly, bin_set)}
