bec7537ee2bdbbfae463db0c5a323f1f1ec979f5
Mo Ch <mocharrout@gmail.com>
2017-06-11 11:15:22 +0200

    Forgot to commit config change

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = '\x13\xae6\xc7\xc4\xb1\xbf\x164\xe7H\xf5\x7fw9\xd7X\xe0\xb5\x90\xc5\x9c\xce\x01'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+# SQLALCHEMY_DATABASE_URI = 'postgresql://corebin:corebin@localhost/corebin'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
-DEBUG = True
+DEBUG = False
 HOST = '0.0.0.0'
